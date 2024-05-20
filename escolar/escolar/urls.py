4084d2409162a14bb1aa9e68ea75b828e0f364af
LADCH02 <ladch02jun.2@gmail.com>
2024-05-20 01:34:35 -0600

    pagina registro sin funcionalidad

--- escolar/escolar/urls.py
+++ escolar/escolar/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("inicio.urls")),
-    # path('alumnos', include("alumnos.urls"))
+    path('alumnos/', include("alumnos.urls"))
 ]
