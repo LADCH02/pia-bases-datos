61fd909fdeb75f2a4563f7ed99bfa60f7129a9a6
LADCH02 <ladch02jun.2@gmail.com>
2024-05-20 13:41:25 -0600

    pagina de grupo especifica

--- escolar/grupos/urls.py
+++ escolar/grupos/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.grupos, name="grupos"),
     path("especifico", views.grupo_especifico, name="grupo-especifico"),
-    path("<int:matricula>", views.detalle_grupo, name="detalle-grupo"),
+    path("grupo/<str:grupoNombre>", views.detalle_grupo, name="detalle-grupo"),
 ]
--- escolar/grupos/views.py
+++ escolar/grupos/views.py
@@ -39,60 +39,36 @@ def grupos(request):
         return render(request, 'grupos/index.html', {'grupos': grupos_dicc})
 
 def grupo_especifico(request):
-    matricula = request.GET["matricula"]
-    return redirect('detalle-alumno', matricula=matricula)
+    grupo = request.GET["grupo"]
+    return redirect('detalle-grupo', grupoNombre=grupo)
 
-def detalle_grupo(request, matricula):
-    if request.method == "GET":
-        cursor = connection.cursor()
-        cursor.execute("""
-            MostrarAlumnoPorMatricula @Matricula = %s
-        """, [matricula])
-        alumnos = cursor.fetchall()
-        connection.close()
-        
-        alumno = lista_grupos_a_diccionarios(alumnos)[0]
-        cantidad_apellidos = len(alumno["apellidos"].split(" "))
-        if cantidad_apellidos == 1:
-            alumno["apellidoP"] = alumno["apellidos"]
-        if cantidad_apellidos > 1:
-            apellidoP, apellidoM = alumno["apellidos"].split(" ")[:2]
-            alumno["apellidoP"] = apellidoP
-            alumno["apellidoM"] = apellidoM
-        
-        return render(request, "alumnos/detalle-alumno.html", {'alumno': alumno})
+def detalle_grupo(request, grupoNombre):
+    cursor = connection.cursor()
+    cursor.execute("""
+        SELECT IdGrupo, Nombre FROM Grupos WHERE Nombre = %s
+    """, [grupoNombre])
+    grupos = cursor.fetchall()
 
-    if request.method == "POST":
-        matriculaNueva = request.POST['matricula']
-        nombre = request.POST['nombre']
-        apellidoP = request.POST['apellidoP']
-        apellidoM = request.POST['apellidoM']
-        correo = request.POST['correo']
-        celular = request.POST['celular']
-        grupo = request.POST['grupo']
-        carrera = request.POST['carrera']
-        semestre = request.POST['semestre']
-        
-        cursor = connection.cursor()
+    grupos_dicc = lista_grupos_a_diccionarios(grupos) # solo retorna un grupo
+    grupo = grupos_dicc[0]
+    
+    grupo["alumnos"] = []
+    grupo["maestros"] = []
+    # obtener alumnos del grupo
+    cursor.execute(
+        """SELECT Matricula, Nombre, Apellidos FROM Alumnos WHERE Grupo = %s""", 
+        [grupo["nombre"]]
+    )
+    alumnos = lista_data_a_diccionarios(cursor.fetchall(), ["matricula", "nombre", "apellidos"])
+    grupo["alumnos"] += alumnos
+    
+    # obtener maestros del grupo
+    cursor.execute(
+        """SELECT NumEmpleado, Nombre, Apellidos FROM Maestros WHERE Grupo = %s""", 
+        [grupo["nombre"]]
+    )
+    maestros = lista_data_a_diccionarios(cursor.fetchall(), ["numEmpleado", "nombre", "apellidos"])
+    grupo["maestros"] += maestros
+    
+    return render(request, "grupos/detalle-grupo.html", {'grupo': grupo})
 
-        try:
-            cursor.execute("""
-                UPDATE Alumnos
-                SET Matricula = %s,
-                    Nombre = %s,
-                    Apellidos = %s,
-                    Correo = %s,
-                    Celular = %s,
-                    IdCarrera = (SELECT IdCarrera FROM Carreras WHERE Nombre = %s),
-                    Semestre = %s,
-                    Grupo = (SELECT IdGrupo FROM Grupos WHERE Nombre = %s)
-                WHERE Matricula = %s
-            """, (matriculaNueva, nombre, apellidoP + " " + apellidoM, correo, celular, carrera, semestre, grupo, matricula))
-            connection.commit()
-        except Exception as e:
-            connection.rollback()
-            return render(request, 'alumnos/detalle-alumno.html', {'error': str(e)})
-        finally:
-            connection.close()
-
-        return redirect(reverse("alumnos"))
