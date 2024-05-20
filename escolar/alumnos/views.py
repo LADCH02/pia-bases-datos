7b847abc5282a6b5a70d03122e6408be60252a40
LADCH02 <ladch02jun.2@gmail.com>
2024-05-20 02:56:20 -0600

    registro funcional con sql

--- escolar/alumnos/views.py
+++ escolar/alumnos/views.py
@@ -1,6 +1,29 @@
-from django.shortcuts import render
+from django.db import connection
+from django.shortcuts import redirect, render
+from django.urls import reverse
+from django.contrib.messages import add_message
 
 # Create your views here.
 
 def registro(request):
-    return render(request, "alumnos/registro.html")
+    if request.method == "GET":
+        return render(request, "alumnos/registro.html")
+    if request.method == "POST":
+        matricula = request.POST['matricula']
+        nombre = request.POST['nombre']
+        apellidoP = request.POST['apellidoP']
+        apellidoM = request.POST['apellidoM']
+        correo = request.POST['correo']
+        celular = request.POST['celular']
+        grupo = request.POST['grupo']
+        carrera = request.POST['carrera']
+        semestre = request.POST['semestre']
+
+        with connection.cursor() as cursor:
+            # make a valid cursor.execute
+            cursor.execute("""
+                INSERT INTO Alumnos (matricula, Nombre, Apellidos, Correo, Celular, IdCarrera, Semestre, Grupo)
+                VALUES (%s, %s, %s, %s, %s, (SELECT IdGrupo FROM Grupos WHERE Nombre = %s), %s, (SELECT IdGrupo FROM Grupos WHERE Nombre = %s))
+            """, (matricula, nombre, apellidoP + " "+ apellidoM, correo, celular, carrera, semestre, grupo))
+
+        return redirect(reverse("inicio"))
--- escolar/escolar/settings.py
+++ escolar/escolar/settings.py
@@ -79,11 +79,12 @@ WSGI_APPLICATION = 'escolar.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mssql',
+        'NAME': 'escuela',
+        'HOST': 'DESKTOP-1CPKVEQ',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
