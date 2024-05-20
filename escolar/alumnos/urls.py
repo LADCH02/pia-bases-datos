83d075576fc19009af4e6f0b762a6dd7dc888d2b
LADCH02 <ladch02jun.2@gmail.com>
2024-05-20 04:39:47 -0600

    pagina de edicion de alumnos

--- escolar/alumnos/urls.py
+++ escolar/alumnos/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("registro", views.registro, name="alumnos-registro"),
     path("", views.alumnos, name="alumnos"),
+    path("<int:matricula>", views.detalle_alumno, name="detalle-alumno"),
 ]
--- escolar/alumnos/views.py
+++ escolar/alumnos/views.py
@@ -53,3 +53,62 @@ def alumnos(request):
         print(alumnos_dicc)
 
         return render(request, 'alumnos/index.html', {'alumnos': alumnos_dicc})
+    
+def detalle_alumno(request, matricula):
+    if request.method == "GET":
+        cursor = connection.cursor()
+        cursor.execute("""
+            SELECT Matricula, Nombre, Apellidos, Correo, Celular,
+                (SELECT Nombre FROM Carreras WHERE Carreras.IdCarrera = Alumnos.IdCarrera) AS Carrera,
+                Semestre,
+                (SELECT Nombre FROM Grupos WHERE Grupos.IdGrupo = Alumnos.Grupo) AS Grupo
+            FROM Alumnos WHERE Matricula = %s
+        """, [matricula])
+        alumnos = cursor.fetchall()
+        connection.close()
+        
+        alumno = lista_alumnos_a_diccionarios(alumnos)[0]
+        cantidad_apellidos = len(alumno["apellidos"].split(" "))
+        if cantidad_apellidos == 1:
+            alumno["apellidoP"] = alumno["apellidos"]
+        if cantidad_apellidos > 1:
+            apellidoP, apellidoM = alumno["apellidos"].split(" ")[:2]
+            alumno["apellidoP"] = apellidoP
+            alumno["apellidoM"] = apellidoM
+        
+        return render(request, "alumnos/detalle-alumno.html", {'alumno': alumno})
+
+    if request.method == "POST":
+        matriculaNueva = request.POST['matricula']
+        nombre = request.POST['nombre']
+        apellidoP = request.POST['apellidoP']
+        apellidoM = request.POST['apellidoM']
+        correo = request.POST['correo']
+        celular = request.POST['celular']
+        grupo = request.POST['grupo']
+        carrera = request.POST['carrera']
+        semestre = request.POST['semestre']
+        
+        cursor = connection.cursor()
+
+        try:
+            cursor.execute("""
+                UPDATE Alumnos
+                SET Matricula = %s,
+                    Nombre = %s,
+                    Apellidos = %s,
+                    Correo = %s,
+                    Celular = %s,
+                    IdCarrera = (SELECT IdCarrera FROM Carreras WHERE Nombre = %s),
+                    Semestre = %s,
+                    Grupo = (SELECT IdGrupo FROM Grupos WHERE Nombre = %s)
+                WHERE matricula = %s
+            """, (matriculaNueva, nombre, apellidoP + " " + apellidoM, correo, celular, carrera, semestre, grupo, matricula))
+            connection.commit()
+        except Exception as e:
+            connection.rollback()
+            return render(request, 'alumnos/detalle-alumno.html', {'error': str(e)})
+        finally:
+            connection.close()
+
+        return redirect(reverse("alumnos"))
